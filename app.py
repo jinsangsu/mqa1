805f17e5d68120ecc679af2ea4f3b2b09d3b3305
jinsangsu <licjssj777@gmail.com>
2025-07-20 16:39:15 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -135,6 +135,7 @@ if search_query.strip() or search_writer.strip():
                         new_question = st.text_area("질문 내용", value=row["질문"])
                         new_answer = st.text_area("답변 내용", value=row["답변"])
                         new_writer = st.text_input("작성자", value=row["작성자"])
+                        submitted = st.form_submit_button("저장")
                         if st.form_submit_button("저장", key=f"save_{idx}_{row['rowid']}"):
                          st.write("저장 버튼 눌림!")  # 1. 버튼 동작 확인
                          try:    
