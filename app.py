2770c906f201c3ff3de726268d527666026c7527
jinsangsu <licjssj777@gmail.com>
2025-07-28 10:42:01 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -75,7 +75,11 @@ with container:
             <p style="margin-top: 8px;"><strong>안녕하세요.</strong></p>
             <p><strong>항상 현장에서 최선을 다해주셔서 깊이 감사드립니다.</strong></p>
             <p><strong>영업가족분들이 자주하는 질문에 답변하는 Q&A 시스템을 준비했습니다.</strong></p>
-            <p><strong> (앱주소 : http://chung2.streamlit.app)</strong></p>
+            <p><strong>
+    (<a href="http://chung2.streamlit.app" target="_blank" style="color: red; text-decoration: none;">
+        앱주소 : http://chung2.streamlit.app
+    </a>)
+</strong></p>
             <p><strong>영업가족분들이 자주하는 질문들을 등록해주시면</strong></p>
             <p><strong>우리 충청호남본부의 변화와 성장에 큰 기여가 될 것입니다.</strong></p>
             <p><strong>감사합니다</strong></p>
